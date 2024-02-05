72be3aa0115e1995198ec0a3d0ee5a62b65432e4
nutpp <nut230843@gmail.com>
2024-02-05 19:05:06 +0700

    check_route_loginUser

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,14 +1,20 @@
 import User from "../models/userModel.js"
 import HttpError from "../models/errorModel.js"
 import bcrypts from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+import path from "path";
+import { v4 as uuidv4, v4 } from "uuid";
+
 // ============REGISTER A NEW USER
 // POST : api/user/register 
 // UNPROTECTED
 const resgisterUser = async (req, res, next) => {
     try {
         const { name, email, password, password2 } = req.body;
-        if (!name || !email || !password || !password) {
-            return next(new HttpError('Fill in all fields',422));
+        if (!name || !email || !password) {
+            // console.log(email);
+            return next(new HttpError('Fill in all fields', 422));
         }
         const newEmail = email.toLowerCase();
         const emailExists = User.findOne({ email: newEmail });
@@ -17,17 +23,17 @@ const resgisterUser = async (req, res, next) => {
             next(new HttpError('Email already exists'));
         }
 
-        if ((password.trim().length) < 6){
-            return next(new HttpError ('Password should be least 6 characters.',422));
+        if ((password.trim().length) < 6) {
+            return next(new HttpError('Password should be least 6 characters.', 422));
         }
 
-        if(password !== password2){
-            return next(new HttpError('Password do not match.',422))
+        if (password !== password2) {
+            return next(new HttpError('Password do not match.', 422))
         }
 
         const salt = await bcrypts.genSalt(10);
-        const hashedPass = await bcrypts.hash(password,salt);
-        const newUser = await User.create({name,email:newEmail,password:hashedPass});
+        const hashedPass = await bcrypts.hash(password, salt);
+        const newUser = await User.create({ name, email: newEmail, password: hashedPass });
 
         res.status(201).json(newUser);
     } catch (error) {
@@ -40,7 +46,27 @@ const resgisterUser = async (req, res, next) => {
 // POST : api/user/register 
 // UNPROTECTED
 const loginUser = async (req, res, next) => {
-    res.json('Login User')
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return next(new HttpError('Fill in all field.', 422));
+        }
+        const newEmail = email.toLowerCase()
+        const user = await User.findOne({ email: newEmail });
+        if (!user) {
+            return next(new HttpError('Invalid credentails.', 422))
+        }
+        const comparePass = await bcrypts.compare(password, user.password);
+        if (!comparePass) {
+            return next(new HttpError('Invalid credentail', 422));
+        }
+        const { _id: id, name } = user;
+        const token = jwt.sign({ id, name }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        res.status(200).json({ token, id, name });
+
+    } catch (error) {
+        return next(new HttpError('Login failed. Please check your credentails.'))
+    }
 }
 
 
@@ -48,7 +74,17 @@ const loginUser = async (req, res, next) => {
 // GET : api/user/:id
 // PROTECTED
 const getUser = async (req, res, next) => {
-    res.json('User Profile')
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id).select('-password');
+        if (!user) {
+            return next(new HttpError('User not found', 404));
+        }
+        res.status(200).json(user);
+
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
 
 
@@ -56,7 +92,46 @@ const getUser = async (req, res, next) => {
 // POST : api/user/change-avatar
 // PROTECTED
 const changeAvatar = async (req, res, next) => {
-    res.json('Change User Avatar')
+    try {
+        if (!req.files.avatar) {
+            return next(new HttpError('Please choose an Image.', 422));
+        }
+
+        //find user from database
+        const user = await User.findById(req.user.id);
+
+        //delete old avatar if exists
+        if (user.avatar) {
+            fs.unlink(path.join(__dirname, '..', 'uploads', user.avatar, (err) => {
+                if (err) {
+                    return next(new HttpError(err))
+                }
+            }))
+        }
+
+        const {avatar} = req.files;
+        if(avatar.size > 500000){
+            return next(new HttpError('Profile picture too big. should be less than 500kb.',422))
+        }
+        let fileName;
+        fileName = avatar.name;
+        let splitedFileName = fileName.split('.');
+        console.log(splitedFileName);
+        let newFileName = splitedFileName[0] + v4() + '.' + splitedFileName[splitedFileName.length -1];
+        avatar.mv(path.join(__dirname+ '..'+'uploads'+ newFileName, async (err)=>{
+            if (err) {
+                return new HttpError(err)
+            }
+            const uploadedAvatar = await User.findByIdAndUpdate(req.user.id,{avatar:newFileName},{new:true});
+
+            if (!uploadedAvatar) {
+                return new HttpError("Avatar couldn't be changed.",422)
+            }
+        }))
+
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
 
 
@@ -72,7 +147,12 @@ const editUser = async (req, res, next) => {
 // POST : api/user/authors
 // PROTECTED
 const getAuthors = async (req, res, next) => {
-    res.json('Get All user/Author')
+    try {
+        const authors = await User.find().select('-password');
+        res.status(200).json(authors)
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
 
 export {
--- server/index.js
+++ server/index.js
@@ -5,10 +5,20 @@ import "dotenv/config";
 import userRoutes from './routes/userRoutes.js' ;
 import postRoutes from './routes/postRoutes.js' ;
 import { notFound,errorHanddle } from "./middleware/errorMiddleware.js";
+import fileUpload from "express-fileupload";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+
 app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}));
 app.use(cors({credential:true,origin:"http://loaclhost:3000"}));
+app.use(fileUpload());
+app.use('/upload',express.static(__dirname + '/uploads'));
 
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
